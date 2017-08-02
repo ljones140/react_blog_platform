6ec2ccb619812e9e98effa1206b099d5bd76c4f7
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-08-02 21:52:07 +0100

    Form fields created with validation on touched

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,13 +11,20 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.touched ? field.meta.error : ''}
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -33,11 +40,29 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!"
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories"
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please"
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
