b157b8c4b293b67b7888e7ba592985e22bb63fb5
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-08-04 11:48:15 +0100

    Able to navigate to posts via posts/:id

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -7,8 +7,12 @@ class PostsShow extends Component {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
+
   render() {
     const { post } = this.props;
+    if (!post) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
         <h3>{post.title}</h3>
